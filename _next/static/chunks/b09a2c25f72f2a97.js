3afbedfd1337e860d578c8219ed879f11f2e58ad
PhucVu161 <phucvuhuu1601@gmail.com>
2025-10-06 01:45:26 +0700

    Updates

---
+++ _next/static/chunks/b09a2c25f72f2a97.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/3e7689bcb946c40a.js",
+  "static/chunks/turbopack-dd81c29aa1a1c62c.js"
+])
---
+++ _next/static/nCsa4wV9Gd1-MtSD9Gv40/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/b09a2c25f72f2a97.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
